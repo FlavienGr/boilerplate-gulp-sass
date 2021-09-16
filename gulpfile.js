5a1a7ff2ddf5dd0ffd0d750d0915519d2a404da2
FlavGr <zencles75@gmail.com>
2021-09-16 07:16:34 +0200

    add gulp-sass gulp-postcss cssnano gulp-terser browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,60 @@
-export default (cb) => {
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("node-sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
+
+/**
+ * sass task
+ */
+const scssTask = () => {
+  return src("src/styles/styles.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest("dist", { sourcemaps: "." }));
+};
+
+/**
+ * javascript task
+ */
+const jsTask = () => {
+  return src("src/index.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist", { sourcemaps: "." }));
+};
+
+/**
+ * browsersync task
+ */
+const browsersyncServer = (cb) => {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
   cb();
 };
+
+/**
+ * browsersync reload
+ */
+const browsersyncReload = (cb) => {
+  browsersync.reload();
+  cb();
+};
+
+/**
+ * Watch tasks
+ */
+const watchTask = () => {
+  watch("*.html", browsersyncReload);
+  watch(
+    ["src/styles/**/*.scss", "src/js/**/*.js"],
+    series(scssTask, jsTask, browsersyncServer)
+  );
+};
+/**
+ * default gulp task
+ */
+exports.default = series(scssTask, jsTask, browsersyncServer, watchTask);
